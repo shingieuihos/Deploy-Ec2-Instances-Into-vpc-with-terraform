dd1e28ec04cf0ed605eaa6b808673f44c919bf38
ITWithShingi <smudyirwa@gmail.com>
2025-05-29 06:26:53 +0200

    Update main.tf

--- Deploy Ec2/main.tf
+++ Deploy Ec2/main.tf
@@ -85,13 +85,6 @@ resource "aws_security_group" "shingi_nv_office" {
   }
 }
 
-resource "aws_instance" "web_instance" {
-  ami                         = "ami-0fa71268a899c2733"  # Windows Server Core 2025
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.web_servers.id
-  key_name                    = aws_key_pair.shingi_key_pair.key_name
-  vpc_security_group_ids      = [aws_security_group.shingi_nv_office.id]
-  associate_public_ip_address = true
 
   user_data = <<-EOF
     <powershell>
@@ -124,14 +117,7 @@ resource "aws_instance" "web_instance" {
     Name = "SNVO1-Web-Server"
   }
 }
-resource "aws_instance" "accounting_instance" {
-  ami                         = "ami-0596166f97ee983d1"
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.accounting.id
-  key_name                    = aws_key_pair.shingi_key_pair.key_name
-  vpc_security_group_ids      = [aws_security_group.shingi_nv_office.id]
-  associate_public_ip_address = true
-
+  
   user_data = <<-EOF
     <powershell>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
